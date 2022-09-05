7e689e2587b32f1da9bf565f5283bd5d8e7f30e6
Dmitriy_Klaes <89119584059@mail.ru>
2022-09-06 00:41:48 +0300

    Домашняя работа к семинару от 22-09-01

--- src/main/java/ru/Geekbrains/Task000.java
+++ src/main/java/ru/Geekbrains/Task000.java
@@ -11,23 +11,26 @@ import java.util.Arrays;
 public class Task000 {
     public static void main(String[] args) {
         int[] nums = {1, 7, 3, 6, 5, 6};
-        int left_values = nums[0];
-        int right_values = 0;
+        int left_values = 0;
+        int right_values;
         int result_index = 0;
+        int summ_elements = 0;
+        for (int num : nums) {
+            summ_elements += num;
+        }
         for (int i = 1; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                right_values += nums[j];
-            }
+            left_values += nums[i - 1];
+            right_values = summ_elements - left_values - nums[i];
             if (left_values == right_values) {
                 result_index = i;
                 break;
             }
-            else {
-                right_values = 0;
-            }
-            left_values += nums[i];
         }
-        String result_index_str = Integer.toString(result_index);
-        System.out.println("Искомый индекс в массиве " + Arrays.toString(nums) + " = " + result_index_str);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Искомый индекс в массиве ");
+        sb.append(Arrays.toString(nums));
+        sb.append(" = ");
+        sb.append(result_index);
+        System.out.println(sb);
     }
 }
--- src/main/java/ru/Geekbrains/Task001.java
+++ src/main/java/ru/Geekbrains/Task001.java
@@ -7,40 +7,40 @@ import java.util.Arrays;
 
 public class Task001 {
     public static void main(String[] args) {
-        String[] arr = {"aaaaaavv", "aaaadd", "aarrr", "aabb", "aabbb", "aaabb"};
+        String[] arr = {"aaaaaavv", "aaaadd", "aaarrr", "aabb", "aaabbb", "aabb"};
         String result = longestCommonPrefix(arr);
-        if (result != "") {
-            System.out.println("Наиболее длинный общий префикс в массиве " + Arrays.toString(arr) + " - " + result);
+        StringBuilder sb = new StringBuilder();
+        if (!result.equals("")) {
+            sb.append("Наиболее длинный общий префикс в массиве ");
+            sb.append(Arrays.toString(arr));
+            sb.append(" - ");
+            sb.append(result);
         }
         else {
-            System.out.println("Одинаковых префиксов в массиве " + Arrays.toString(arr) + " нет!");
+            sb.append("Одинаковых префиксов в массиве ");
+            sb.append(Arrays.toString(arr));
+            sb.append(" нет!");
         }
+        System.out.println(sb);
     }
 
     static String longestCommonPrefix(String[] array) {
-        int[] arrayPrefixLength = new int[array.length];
-        int count = 0;
-        char singlePrefix = array[0].charAt(0);
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length(); j++) {
-                count++;
-                if (array[i].charAt(j) != array[i].charAt(j + 1)) {
-                    arrayPrefixLength[i] = count;
-                    count = 0;
+        String result = array[0];
+        int index;
+        int lastSymbol;
+        for(int i = 1; i < array.length; i++) {
+            index = 0;
+            lastSymbol = 0;
+            while(index < result.length() && index < array[i].length()) {
+                if(result.charAt(index) == array[i].charAt(index)) {
+                    lastSymbol++;
+                } else {
                     break;
                 }
+                index++;
             }
+            result = result.substring(0, lastSymbol);
         }
-        Arrays.sort(arrayPrefixLength);
-        for (int i = arrayPrefixLength.length - 1; i > 0; i--) {
-            if (arrayPrefixLength[i] == arrayPrefixLength[i - 1]) {
-                StringBuilder sb = new StringBuilder();
-                for(int j = 0; j < arrayPrefixLength[i]; j++) {
-                    sb.append(singlePrefix);
-                }
-                return sb.toString();
-            }
-        }
-        return "";
+        return result;
     }
 }
